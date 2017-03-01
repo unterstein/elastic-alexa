5a450bf71c6ddba690e8efe141c6bd2de77a0854
Johannes Unterstein <junterstein@mesosphere.io>
2017-03-01 13:56:24 +0100

    introduced elastic search client

---
+++ src/main/java/io/voiceops/elastic/ElasticSearchClient.java
@@ -0,0 +1,31 @@
+package io.voiceops.elastic;
+
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+class ElasticSearchClient {
+  private final TransportClient client;
+
+  ElasticSearchClient(String host, int port, String clusterName) {
+    Settings settings = Settings.builder()
+        .put("cluster.name", clusterName).build();
+    try {
+      client = new PreBuiltTransportClient(settings)
+          .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
+    } catch (UnknownHostException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+
+  long count(String term) {
+    return client.prepareSearch().setQuery(QueryBuilders.simpleQueryStringQuery(term)).get().getHits().totalHits();
+  }
+}
